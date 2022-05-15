506b5bad48a071c4ef9e1315fbc51dd9c3665a26
Dipaksinh2 <singhdk.singh68@gmail.com>
2022-05-15 14:12:16 +0530

    Please check

---
+++ xyz.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class EMP
+{
+	public static void main(String[] args) 
+	{
+
+		Scanner add=new Scanner(System.in);
+
+		System.out.print("Eneter Name :-");
+
+		String name=add.next();
+	
+		System.out.println("Your Name :-"+name);
+
+	}
+
+}
