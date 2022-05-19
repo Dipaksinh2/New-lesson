6d09c19bc55cfbb16b0652d1060c68d6a4af838e
Deepak Singh <singhdk.singh68@gmail.com>
2022-05-19 18:14:24 +0530

    Nohello

---
+++ Nohello.java
@@ -0,0 +1,28 @@
+class Demo
+{
+	public static void main(String args[])
+	{
+	
+		System.out.println("HI Demo class  ");
+		A a =new A();
+		System.out.println("HI Just to Check ");
+	}
+}
+class A
+{
+	A()
+	{
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
+class B
+{
+	B()
+	{
+		System.out.println("B class ");
+	}
+}
+modify my Hello,java
