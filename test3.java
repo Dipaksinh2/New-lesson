2f7da8865050e18a5dec919164dbe78a570f8a84
Deepak Singh <singhdk.singh68@gmail.com>
2022-05-31 14:20:56 +0530

    test3.java jst commited

---
+++ test3.java
@@ -0,0 +1,32 @@
+class testing
+{
+		public static void main(String args[])
+		{
+			{
+				System.out.println("Lets Check the value");
+				A a=new A();
+				System.out.println(A);
+			}
+		
+		}
+
+}
+
+
+class A
+{
+		A()
+		{
+
+		}
+
+		Void as()
+		{
+			for(int i=0;i<=4;i++)
+			{
+				System.out.println(i);
+			}
+		
+		}
+
+}
